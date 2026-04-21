9531f0c10acf24bdc7611bf0f049fcb20daabede
CescoCal <terencek@cescoaustralia.com>
2026-04-21 16:19:53 +1000

    Fix wildcard route for Express 5

--- index.js
+++ index.js
@@ -472,8 +472,8 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
 
 /* -----------------------------
